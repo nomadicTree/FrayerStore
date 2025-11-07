a1bee1dbd50e15fcd77963ae6a22dda558c3acc3
= <=>
2025-11-07 20:46:15 +0000

    add page to view words given id in url

---
+++ pages/4_View.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from app_lib.models import Word
+from app_lib.repositories import get_word_by_id
+
+PAGE_TITLE = "View"
+st.set_page_config(page_title=f"FrayerStore | {PAGE_TITLE}", page_icon="🔎")
+
+query_params = st.query_params
+id_param = query_params.get("id")  # returns a string or None
+
+if id_param is None:
+    st.info("No id given in URL")
+else:
+    try:
+        word_id = int(id_param)
+    except ValueError:
+        st.error("Invalid id in URL")
+        st.stop()
+    word_row = get_word_by_id(word_id)
+    if word_row is None:
+        st.error(f"No word found with id {word_id}")
+        st.stop()
+    word = Word(word_row)
+    word.display_frayer(show_subject=True, show_topics=True)
