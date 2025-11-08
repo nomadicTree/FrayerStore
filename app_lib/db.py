3ec206f393f8c7f021e6bf19616ddbfdd5910680
= <=>
2025-11-08 11:20:30 +0000

    docstring and type hint

--- app_lib/db.py
+++ app_lib/db.py
@@ -8,7 +8,12 @@ DB_PATH = os.path.join(
 
 
 @st.cache_resource
-def get_db():
+def get_db() -> sqlite3.Connection:
+    """Open a cached connection to the database
+
+    Returns:
+        A connection object to the application's SQLite database.
+    """
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
