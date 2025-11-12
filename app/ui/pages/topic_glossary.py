5458ddaed26067231654a4cfdc01a40e4177eb5c
= <=>
2025-11-12 18:17:35 +0000

    moved

---
+++ app/ui/pages/topic_glossary.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from app.ui.components.page_header import page_header
+
+PAGE_TITLE = "Topic Glossary"
+
+
+def main():
+    page_header(PAGE_TITLE)
+
+
+if __name__ == "__main__":
+    main()
