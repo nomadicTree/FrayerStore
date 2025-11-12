4f01f0f4121edc00f3d7ab5342a2173f2fe50658
= <=>
2025-11-12 18:13:15 +0000

    Set default value for description parameter in Level constructor

--- app/core/models/level_model.py
+++ app/core/models/level_model.py
@@ -1,5 +1,5 @@
 class Level:
-    def __init__(self, level_id: int, name: str, description: str):
+    def __init__(self, level_id: int, name: str, description: str = None):
         self.level_id = level_id
         self.name = name
         self.description = description
