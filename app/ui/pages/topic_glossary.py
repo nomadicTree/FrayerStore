8bf4206c330f1606be33ad3efe208caebf57f3f6
= <=>
2025-11-15 21:34:25 +0000

    new button

--- app/ui/pages/topic_glossary.py
+++ app/ui/pages/topic_glossary.py
@@ -5,7 +5,7 @@ from app.core.respositories.courses_repo import get_courses
 from app.core.respositories.topics_repo import get_topics_for_course
 from app.core.respositories.words_repo import get_word_versions_for_topic
 from app.ui.components.frayer import render_frayer_model
-from app.ui.components.buttons import details_button
+from app.ui.components.buttons import wordversion_details_button
 
 PAGE_TITLE = "Topic Glossary"
 
@@ -16,14 +16,14 @@ def main():
         course = select_course(all_courses)
     page_header(PAGE_TITLE, f"**{course.subject.name}:** {course.name}")
     topics = get_topics_for_course(course, only_with_words=True)
-    for topic in topics:
+    for i, topic in enumerate(topics):
         st.subheader(topic.label)
         topic_word_versions = get_word_versions_for_topic(topic)
         topic_word_versions.sort()
         for wv in topic_word_versions:
             with st.expander(wv.word, expanded=False):
                 render_frayer_model(wv)
-                details_button(wv.url)
+                wordversion_details_button(wv, key_prefix=i)
 
 
 if __name__ == "__main__":
