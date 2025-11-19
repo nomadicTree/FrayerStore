1e70886ca3dac57985d8d32263c1e9d3e5652bda
= <=>
2025-11-19 16:31:20 +0000

    load settings data and cache accordingly

--- frayerstore/app/cache/db.py
+++ frayerstore/app/cache/db.py
@@ -2,8 +2,11 @@
 
 import streamlit as st
 from app.core.db import get_db_uncached
+from app.settings import load_settings
+
+settings = load_settings()
 
 
-@st.cache_resource
+@st.cache_resource(ttl=settings.cache.db_connection_ttl)
 def get_db():
     return get_db_uncached
