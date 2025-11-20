597762e839ec0bfe4ba4dfb5bfebe816e792393a
= <=>
2025-11-20 20:40:02 +0000

    functions to get and insert subjects

---
+++ src/frayerstore/importer/db_utils.py
@@ -0,0 +1,41 @@
+import sqlite3
+from .models import ImportSubject
+
+
+def get_subject_by_name(conn: sqlite3.Connection, name: str) -> sqlite3.Row | None:
+    """Return row of matching subject name"""
+    query = """
+    "SELECT * FROM Subjects WHERE name = ?"
+    """
+    row = conn.execute(query, (name,)).fetchone()
+    if row:
+        return row
+    else:
+        return None
+
+
+def get_subject_by_slug(conn: sqlite3.Connection, slug: str) -> sqlite3.Row | None:
+    """Return row of matching subject slug"""
+    query = """
+    "SELECT * FROM Subjects WHERE slug = ?"
+    """
+    row = conn.execute(query, (slug,)).fetchone()
+    if row:
+        return row
+    else:
+        return None
+
+
+def insert_subject(conn: sqlite3.Connection, incoming: ImportSubject) -> sqlite3.Row:
+    query = """
+    INSERT INTO Subjects (name, slug)
+    VALUES (?, ?)
+    """
+    cur = conn.execute(
+        query,
+        (
+            incoming.name,
+            incoming.slug,
+        ),
+    )
+    return cur.lastrowid
