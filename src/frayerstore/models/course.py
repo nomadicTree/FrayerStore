5e58e03702142cba9afb52408bd873bb4cb7302e
= <=>
2025-11-22 21:05:44 +0000

    revert to use objects rather than pks

--- src/frayerstore/models/course.py
+++ src/frayerstore/models/course.py
@@ -1,19 +1,21 @@
 from dataclasses import dataclass
 from frayerstore.models.domain_entity import DomainEntity
+from frayerstore.models.subject import Subject
+from frayerstore.models.level import Level
 
 
 @dataclass(frozen=True, order=False)
 class Course(DomainEntity):
-    subject_pk: int
-    level_pk: int
     name: str
     slug: str
+    subject: Subject
+    level: Level
 
     def __lt__(self, other: object) -> bool:
         if not isinstance(other, Course):
             return NotImplemented
-        if self.level_pk != other.level_pk:
-            return self.level_pk < other.level_pk
+        if self.level != other.level:
+            return self.level < other.level
 
         return self.name.lower() < other.name.lower()
 
