934e5e25c97eb7080fe58e25c84586bc6622f2fa
= <=>
2025-11-23 13:45:16 +0000

    lt check for level

--- src/frayerstore/models/level.py
+++ src/frayerstore/models/level.py
@@ -7,3 +7,12 @@ from frayerstore.models.domain_entity import DomainEntity
 class Level(DomainEntity):
     name: str
     slug: str
+
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, Level):
+            return NotImplemented
+        self_name = self.name.lower()
+        other_name = other.name.lower()
+        if self_name != other_name:
+            return self_name < other_name
+        return self.pk < other.pk
